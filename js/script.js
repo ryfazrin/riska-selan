2baf189d7c710c25d3e71545587e5abf930641b9
ryfazrin <riansera515@gmail.com>
2023-02-13 17:00:04 +0700

    updated: merge from ryfazrin.github.io

--- js/script.js
+++ js/script.js
@@ -9,3 +9,22 @@ AOS.init({
   delay: 200,
   duration: 1000
 });
+
+function scrollToBottom(timedelay=0) {
+  var scrollId;
+  var height = 0;
+  var minScrollHeight = screen.height;
+  scrollId = setInterval(function () {
+      if (height <= document.body.scrollHeight) {
+          window.scrollBy(0, minScrollHeight);
+      }
+      else {
+          clearInterval(scrollId);
+      }
+      height += minScrollHeight;
+  }, timedelay);           
+}
+
+// setInterval(() => {
+scrollToBottom(2000)
+// }, 1000)
