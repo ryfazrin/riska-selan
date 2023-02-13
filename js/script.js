e2730cd77875e46b8b474b3cbab53a6241dd38f5
ryfazrin <riansera515@gmail.com>
2023-02-13 19:25:47 +0700

    added: first auto scroll

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
+let audio = document.getElementById("audio");
+
 // audio
 function play() {
-	var audio = document.getElementById("audio");
 	audio.play();
+  audio.ontimeupdate = (event) => {
+    let audioTime = document.getElementById("audioTime");
+    audioTime.innerHTML = audio.currentTime;
+    if (Math.floor(audio.currentTime) === 8) {
+      scrollToBottom()
+    }
+    console.log('The currentTime attribute has been updated. Again.');
+  };
+  // scrollToBottom(2000);
+}
+
+let audioPause = document.getElementById("audioPause");
+audioPause.onclick = (event) => {
+  audio.pause();
+}
+
+let audioPlay = document.getElementById("audioPlay");
+audioPlay.onclick = (event) => {
+  audio.play();
 }
 
 // Animate On Scroll Library : https://michalsnik.github.io/aos/
@@ -14,19 +34,28 @@ function scrollToBottom(timedelay = 0) {
   let scrollId;
   let height = 0;
   let minScrollHeight = window.innerHeight; // 100vh
+
+  if (height <= document.body.scrollHeight) {
+    window.scrollBy(0, minScrollHeight);
+    console.log(minScrollHeight);
+  }
+  else {
+      clearInterval(scrollId);
+  }
+
+height += minScrollHeight;
   
-  scrollId = setInterval(function () {
-      if (height <= document.body.scrollHeight) {
-          window.scrollBy(0, minScrollHeight);
-          console.log(minScrollHeight);
-      }
-      else {
-          clearInterval(scrollId);
-      }
-      height += minScrollHeight;
-  }, timedelay);           
+  // scrollId = setInterval(function () {
+  //     if (height <= document.body.scrollHeight) {
+  //         window.scrollBy(0, minScrollHeight);
+  //         console.log(minScrollHeight);
+  //     }
+  //     else {
+  //         clearInterval(scrollId);
+  //     }
+  //     height += minScrollHeight;
+  // }, timedelay);           
 }
 
 // setInterval(() => {
-scrollToBottom(2000)
 // }, 1000)
